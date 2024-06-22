3e9736197b2b6ac734258c8200fc1847c347cd0a
MZecevic@suitsupply.com <marko_zecevic@epam.com>
2024-06-22 15:30:25 +0200

    Fix language selector for mobile

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer';
 import { TranslationsWrapper as Header } from '@/components/Header/TranslationsWrapper';
 import { Inter } from 'next/font/google';
-import '../globals.css';
+import '../globals.scss';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
