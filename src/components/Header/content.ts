5821347350e8e2b4c897674ca7d57ae6a2157bf0
obradz <obradtf@gmail.com>
2024-06-24 15:49:04 +0200

    implement Hero Section

--- src/components/Header/content.ts
+++ src/components/Header/content.ts
@@ -1,33 +1,14 @@
 export const aboutUsLinks = [
-  {
-    name: "o kompaniji",
-    path: "/o-nama/o-kompaniji",
-  },
-  {
-    name: "politika kvaliteta",
-    path: "/o-nama/politika-kvaliteta",
-  },
-  {
-    name: "proizvodnja",
-    path: "/o-nama/proizvodnja",
-  },
-];
-
-export const productLinks = [
-  {
-    name: "kokoši",
-    path: "/proizvodi/kokosi",
-  },
-  {
-    name: "svinje",
-    path: "/proizvodi/svinje",
-  },
-  {
-    name: "goveda",
-    path: "/proizvodi/goveda",
-  },
-  {
-    name: "ćurke",
-    path: "/proizvodi/curke",
-  },
+    {
+        name: 'o kompaniji',
+        path: '/o-nama/o-kompaniji'
+    },
+    {
+        name: 'politika kvaliteta',
+        path: '/o-nama/politika-kvaliteta'
+    },
+    {
+        name: 'proizvodnja',
+        path: '/o-nama/proizvodnja'
+    }
 ];
