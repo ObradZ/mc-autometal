d688fbf54ca5d4c811a1530eedd8979d628b4448
obradz <obradtf@gmail.com>
2024-06-25 18:14:18 +0200

    merge conflicts

--- src/app/[locale]/kontakt/page.tsx
+++ src/app/[locale]/kontakt/page.tsx
@@ -7,6 +7,7 @@ const page = () => {
         <main>
             <HeroSection
                 image={HeroImg}
+                title='Lorem Ipsum'
                 text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies gravida metus ac auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Pellentesque urna diam, imperdiet ac quam consectetur, facilisis aliquam orci. Maecenas laoreet eleifend odio pretium maximus.'
             />
             <Contact />
