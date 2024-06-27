65779a6f8fa48eb3ce98bb3e60411df7cea78b00
obradz <obradtf@gmail.com>
2024-06-27 10:09:38 +0200

    feature(link): implements link and fixes hero section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import { HeroSectionProps } from './types';
 import styles from './HeroSection.module.scss';
 import SectionWrapper from '../SectionWrapper';
-import { Link } from '@/navigation';
 import LearnMoreHeroPart from './LearnMoreHeroPart';
 import CountUpHeroPart from './CountUpHeroPart';
+import MainLink from '../MainLink';
 
 const HeroSection = ({ image, title, text, isLearnMoreVisible = true, countUpData }: HeroSectionProps) => {
     return (
@@ -14,9 +14,7 @@ const HeroSection = ({ image, title, text, isLearnMoreVisible = true, countUpDat
                 <div className={styles.contentWrapper}>
                     <h1 className={styles.title}>{title}</h1>
                     <p className={styles.text}>{text}</p>
-                    <Link className={styles.learnMore} href='/o-nama'>
-                        Saznaj više →
-                    </Link>
+                    <MainLink href='/o-nama'>Saznaj više →</MainLink>
                 </div>
                 {countUpData && <CountUpHeroPart data={countUpData} />}
             </SectionWrapper>
---
+++ src/components/MainLink/types.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+export interface MainLinkProps {
+    href: string;
+    children: ReactNode;
+    superStyles?: string;
+}
