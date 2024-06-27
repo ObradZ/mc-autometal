020a27a1b4684fd21a1a82eddef3998b2c3e3abc
Marko, Zecevic <marczecevic@gmail.com>
2024-06-27 10:50:13 +0200

    Add section wrapper to map and shade prop to heading

--- src/components/MainHeading/index.tsx
+++ src/components/MainHeading/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './MainHeading.module.scss';
-const MainHeading = ({ title, subtitle }: { title: string; subtitle: string }) => {
+
+interface mainHeadingProps {
+    title: string;
+    subtitle: string;
+    shade: 'light' | 'dark';
+    superStyles?: string;
+}
+
+const MainHeading = ({ title, subtitle, shade, superStyles }: mainHeadingProps) => {
     return (
-        <div className={styles.mainHeadingContainer}>
-            <h4 className={styles.subHeading}>{subtitle}</h4>
-            <h3 className={styles.heading}>{title}</h3>
+        <div className={[styles.mainHeadingContainer, superStyles].join(' ')}>
+            <h4 className={shade === 'light' ? styles.subHeadingLight : styles.subHeadingDark}>{subtitle}</h4>
+            <h3 className={shade === 'light' ? styles.headingLight : styles.headingDark}>{title}</h3>
         </div>
     );
 };
