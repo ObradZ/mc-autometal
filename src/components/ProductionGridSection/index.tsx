a2e5fbde9275871c3b0225c09db27bd513db9049
stefan <s.sokolovic@emakina.rs>
2024-06-27 19:54:39 +0200

    production grid section component

---
+++ src/components/ProductionGridSection/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
+import styles from './ProductionGridSection.module.scss';
+import SectionWrapper from '../SectionWrapper';
+import Rectangle from '../../../public/static/images/ProductionGridSection/Rectangle.png';
+import { Link } from '@/navigation';
+
+interface ProductionGridSectionProps {
+    title: string;
+    subtitle: string;
+    images: {
+        src: StaticImageData;
+        alt: string;
+        text: string;
+    }[];
+}
+
+const ProductionGridSection = ({ title, subtitle, images }: ProductionGridSectionProps) => {
+    return (
+        <SectionWrapper>
+            <div className={styles.container}>
+                <h1 className={styles.title}>{title}</h1>
+                <h2 className={styles.subtitle}>{subtitle}</h2>
+                <div className={styles.grid}>
+                    {images.map((image, index) => (
+                        <div key={index} className={styles.gridItem}>
+                            <div className={styles.imageWrapper}>
+                                <Image
+                                    src={image.src}
+                                    alt={image.alt}
+                                    width={300}
+                                    height={200}
+                                    className={styles.image}
+                                />
+                                <div className={styles.overlay}>
+                                    <div className={styles.overlayContent}>
+                                        <p className={styles.overlayText}>{image.text}</p>
+                                        <Link href='#' className={styles.overlayButton}>
+                                            Saznaj više →
+                                        </Link>
+                                    </div>
+                                </div>
+                                <Image className={styles.rectangleImg} src={Rectangle} alt='rectangle' />
+                                <p className={styles.imageCaption}>{image.alt}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </SectionWrapper>
+    );
+};
+
+export default ProductionGridSection;
