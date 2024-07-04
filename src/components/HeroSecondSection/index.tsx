edf79294c51dd9010dea5b1b600f5959d31e615a
obradz <obradtf@gmail.com>
2024-07-04 07:22:46 +0200

    feature(second-hero-section): updates gear images and positions

--- src/components/HeroSecondSection/index.tsx
+++ src/components/HeroSecondSection/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import HeroImage from '../../../public/static/images/HeroSecondSection/hero-second-img-min.jpg';
 import GearImage from '../../../public/static/images/HeroSecondSection/gear.png';
+import HalfGearImage from '../../../public/static/images/HeroSecondSection/halfGear.png';
 import styles from './HeroSecondSection.module.scss';
 import SectionWrapper from '../SectionWrapper';
 import MainHeading from '../MainHeading';
@@ -13,13 +14,13 @@ const HeroSecondSection = () => {
             <Image className={styles.heroImage} src={HeroImage} alt='Banner' />
             <SectionWrapper superStyles={styles.sectionStyles}>
                 <div className={styles.contentWrapper}>
-                    <Image src={GearImage} alt='Gear image' className={styles.gearImage1} />
+                    <Image src={HalfGearImage} alt='Half Gear' className={styles.gearImage1} />
                     <MainHeading title={content.title} subtitle={content.subtitle} shade='light' />
                     <p className={styles.text}>{content.text}</p>
                     <MainLink href='/kontakt' superStyles={styles.link} shade='light'>
                         Kontaktiraj nas →
                     </MainLink>
-                    <Image src={GearImage} alt='Gear image' className={styles.gearImage2} />
+                    <Image src={GearImage} alt='Gear' className={styles.gearImage2} />
                 </div>
             </SectionWrapper>
         </div>
