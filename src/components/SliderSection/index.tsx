02f19b757ac1b883bfb1f7768eb3ae7b0b2eb410
obradz <obradtf@gmail.com>
2024-07-07 10:17:26 +0200

    feature(sectionSlider): adds title and subtitle

--- src/components/SliderSection/index.tsx
+++ src/components/SliderSection/index.tsx
@@ -4,10 +4,12 @@ import { SliderSectionProps } from './types';
 import styles from './SliderSection.module.scss';
 import SectionWrapper from '../SectionWrapper';
 import Slider from './Slider';
+import MainHeading from '../MainHeading';
 
 const SliderSection = ({ slides }: SliderSectionProps) => {
     return (
-        <SectionWrapper>
+        <SectionWrapper superStyles={styles.superStyles}>
+            <MainHeading subtitle='Lorem ipsum' title='Lorem ipsum' shade='dark' />
             <div className={styles.wrapper}>
                 <Slider options={{ loop: true }} slides={slides} />
             </div>
