457096746445143fee655240505f36404ac5720b
Marko, Zecevic <marczecevic@gmail.com>
2024-08-29 12:15:59 +0200

    Add content for cnc laser info

--- src/components/CncLaserInfo/content.ts
+++ src/components/CncLaserInfo/content.ts
@@ -1,5 +1,5 @@
 export const content = {
     title: 'Lorem Ipsum',
     subtitle: 'Lorem Ipsum',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies gravida metus ac auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Pellentesque urna diam, imperdiet ac quam consectetur, facilisis aliquam orci. Maecenas laoreet eleifend odio pretium maximus.'
+    text: 'Dodatno, ovaj laser je opremljen automatski izmjenjivim stolom, što nam omogućava značajne uštede u vremenu pripreme. Ova funkcionalnost omogućava bržu promenu materijala i smanjenje downtime-a između različitih zadataka, čime dodatno povećavamo našu efikasnost i produktivnost.Ova tehnologija nam omogućava da ispunimo čak i najzahtevnije specifikacije naših klijenata, pružajući im komponente visokog kvaliteta sa savršenim ivicama i minimalnim termičkim deformacijama. Naš tim stručnjaka je obučen da maksimalno iskoristi potencijal ovog fiber lasera, što nam omogućava da budemo na čelu inovacija i efikasnosti u industriji mašinske obrade.'
 };
