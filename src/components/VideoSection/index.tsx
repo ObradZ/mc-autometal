255db16d82fedd9e620c8cb85ee1045404a35ff3
Marko, Zecevic <marczecevic@gmail.com>
2024-08-29 13:01:06 +0200

    Fix build error

--- src/components/VideoSection/index.tsx
+++ src/components/VideoSection/index.tsx
@@ -5,20 +5,13 @@ import styles from './VideoSection.module.scss';
 import MainHeading from '../MainHeading';
 import ReactPlayer from 'react-player';
 
-const VideoSection = ({ videoUrl }: { videoUrl: string }) => {
+const VideoSection = ({ videoUrl, videoText }: { videoUrl: string; videoText: string }) => {
     return (
         <SectionWrapper superStyles={styles.videoSection}>
             <div className={styles.videoSectionWrapper}>
                 <div className={styles.videoSectionContent}>
                     <MainHeading title='Lorem ipsum' subtitle='Lorem ipsum' shade='dark' />
-                    <p className={styles.videoSectionParagraph}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, beatae velit ullam molestiae
-                        aliquam libero dolores soluta sequi. Fugit incidunt commodi eveniet nisi, libero, possimus
-                        dignissimos consequatur non sapiente eius explicabo amet! Accusamus, commodi! Voluptatibus
-                        nostrum distinctio harum reprehenderit deleniti ab iusto totam beatae, sint officia deserunt
-                        excepturi assumenda dicta dolorem placeat. Non, dolorum vitae autem dicta, vel molestias
-                        exercitationem, facilis ipsa ad nihil fugiat harum reprehenderit nobis? Deserunt, aperiam.
-                    </p>
+                    <p className={styles.videoSectionParagraph}>{videoText}</p>
                 </div>
                 <div className={styles.videoSectionVideoWrapper}>
                     <ReactPlayer width={'100%'} height={'100%'} volume={0} controls url={videoUrl} />
