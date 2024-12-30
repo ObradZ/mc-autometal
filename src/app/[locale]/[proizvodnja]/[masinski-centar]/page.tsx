9c52fc51b1d59894d3a9b2d36d4f1da892ff282a
Stefan Sokolovic <sokolovic95@gmail.com>
2024-12-30 17:06:50 +0100

    add image section component on machine center page

--- src/app/[locale]/[proizvodnja]/[masinski-centar]/page.tsx
+++ src/app/[locale]/[proizvodnja]/[masinski-centar]/page.tsx
@@ -2,12 +2,13 @@ import HeroSection from '@/components/HeroSection';
 import React from 'react';
 import MasinskiCentar from '@images/MachineCenter/masinski_centar.jpg';
 import { data } from './content';
-import VideoSection from '@/components/VideoSection';
 import SliderSection from '@/components/SliderSection';
 import ProductionGridSection from '@/components/ProductionGridSection';
 import { images } from '@/components/ProductionGridSection/content';
 import HeroSecondSection from '@/components/HeroSecondSection';
 import { useTranslations } from 'next-intl';
+import ImageSection from '@/components/ImageSection';
+import MachineParkImg from '/public/static/images/ImageSection/ImageSection-min.jpg';
 
 const MachineCenter = () => {
     const t = useTranslations('Machine_center');
@@ -22,10 +23,10 @@ const MachineCenter = () => {
                 title={t('hero_section_title')}
                 text={t('hero_section_text')}
             />
-            <VideoSection
-                videoUrl='https://www.youtube.com/watch?v=wTauEVgfcDw'
-                videoText={t('video_section_text')}
-                videoTitle={t('video_section_title')}
+            <ImageSection
+                imageUrl={MachineParkImg}
+                imageText={t('image_section_text')}
+                imageTitle={t('image_section_title')}
             />
             <SliderSection title={cncT('slider_section_title')} />
             <ProductionGridSection title={t('production_grid_section_title')} images={images} />
