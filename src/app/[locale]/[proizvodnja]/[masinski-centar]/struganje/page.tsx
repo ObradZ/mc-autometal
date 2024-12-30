3519527a517949f85fa5616e3dadc37e17372cd0
Marko, Zecevic <marczecevic@gmail.com>
2024-12-30 10:37:59 +0100

    Add translations for production grid cards

--- src/app/[locale]/[proizvodnja]/[masinski-centar]/struganje/page.tsx
+++ src/app/[locale]/[proizvodnja]/[masinski-centar]/struganje/page.tsx
@@ -4,7 +4,6 @@ import ProductionGridSection from '@/components/ProductionGridSection';
 import { images } from '@/components/ProductionGridSection/content';
 import VideoSection from '@/components/VideoSection';
 import React from 'react';
-import Struganje from '@images/MachineCenter/glodanje.jpg';
 import { data } from './content';
 import { useTranslations } from 'next-intl';
 const Page = () => {
