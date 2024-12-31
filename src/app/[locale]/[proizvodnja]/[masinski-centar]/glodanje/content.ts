50391aafa33121a176ca05ea90d72a5c5b99a2f6
Marko, Zecevic <marczecevic@gmail.com>
2024-12-31 11:32:38 +0100

    Add x y z

--- src/app/[locale]/[proizvodnja]/[masinski-centar]/glodanje/content.ts
+++ src/app/[locale]/[proizvodnja]/[masinski-centar]/glodanje/content.ts
@@ -1,7 +1,7 @@
 export const data = [
-    { number: 1600, property: 'mm', id: 0 },
+    { number: 1600, property: 'x', id: 0, isUnits: true },
     { property: 'divider', number: 0, id: 1 },
-    { number: 800, property: 'mm', id: 2 },
+    { number: 800, property: 'y', id: 2, isUnits: true },
     { property: 'divider', number: 0, id: 3 },
-    { number: 800, property: 'mm', id: 4 }
+    { number: 800, property: 'z', id: 4, isUnits: true }
 ];
